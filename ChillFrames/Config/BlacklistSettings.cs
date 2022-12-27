859c1a6105bdf528da4e3ec01d580089cedff75f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 04:10:28 -0800

    Move Condition functions to KamiLib

---
+++ ChillFrames/Config/BlacklistSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using KamiLib.Configuration;
+
+namespace ChillFrames.Config;
+
+public enum BlacklistMode
+{
+    /// <summary>
+    /// Do not limit Framerate in these zones
+    /// </summary>
+    Exclusion,
+
+    /// <summary>
+    /// Only limit framerate in these zones
+    /// </summary>
+    Inclusion
+}
+
+public class BlacklistSettings
+{
+    public Setting<bool> EnabledSetting = new(false);
+    public Setting<BlacklistMode> ModeSetting = new(BlacklistMode.Exclusion);
+
+    public Setting<List<uint>> BlacklistedZones = new(new List<uint>());
+}
