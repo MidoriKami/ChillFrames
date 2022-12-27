2ce4d1c3a72a25c8a39229ee47e1111b2dbf1d4a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 04:13:07 -0800

    Remove unused service objects

--- ChillFrames/Service.cs
+++ ChillFrames/Service.cs
@@ -1,8 +1,5 @@
-﻿using ChillFrames.Data;
-using Dalamud.Data;
+﻿using ChillFrames.Config;
 using Dalamud.Game.ClientState;
-using Dalamud.Game.ClientState.Conditions;
-using Dalamud.Game.Gui;
 using Dalamud.Interface.Windowing;
 using Dalamud.IoC;
 using Dalamud.Plugin;
@@ -12,10 +9,7 @@ namespace ChillFrames;
 public class Service
 {
     [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] public static Condition Condition { get; private set; } = null!;
     [PluginService] public static ClientState ClientState { get; private set; } = null!;
-    [PluginService] public static DataManager DataManager { get; private set; } = null!;
-    [PluginService] public static GameGui GameGui { get; private set; } = null!;
 
     public static WindowSystem WindowSystem { get; } = new("ChillFrames");
     public static Configuration Configuration { get; set; } = null!;
