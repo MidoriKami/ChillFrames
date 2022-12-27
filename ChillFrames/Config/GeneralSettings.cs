931ac8ce238f121302b0b264604c92c009395016
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 04:13:58 -0800

    Update configuration objects to Setting<T>

---
+++ ChillFrames/Config/GeneralSettings.cs
@@ -0,0 +1,17 @@
+﻿using KamiLib.Configuration;
+
+namespace ChillFrames.Config;
+
+public class GeneralSettings
+{
+    public Setting<bool> EnableLimiterSetting = new(true);
+    public Setting<int> FrameRateLimitSetting = new(60);
+
+    public Setting<bool> DisableDuringCutsceneSetting = new(true);
+    public Setting<bool> DisableDuringCombatSetting = new(true);
+    public Setting<bool> DisableDuringDutySetting = new(true);
+    public Setting<bool> DisableDuringQuestEventSetting = new(true);
+    public Setting<bool> DisableDuringCraftingSetting = new(true);
+    public Setting<bool> DisableIslandSanctuarySetting = new(true);
+    public Setting<bool> DisableDuringDutyRecorderPlaybackSetting = new(true);
+}
---
+++ ChillFrames/System/FrameLimiterCondition.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using ChillFrames.Config;
+using Condition = KamiLib.Utilities.Condition;
+
+namespace ChillFrames.System;
+
+internal static class FrameLimiterCondition
+{
+    private static GeneralSettings Settings => Service.Configuration.General;
+    private static BlacklistSettings Blacklist => Service.Configuration.Blacklist;
+
+    public static bool DisableFramerateLimit()
+    {
+        if (Blacklist is {EnabledSetting.Value: true, ModeSetting.Value: BlacklistMode.Exclusion} && InFilteredZone()) return true;
+        if (Blacklist is {EnabledSetting.Value: true, ModeSetting.Value: BlacklistMode.Inclusion} && InFilteredZone()) return false;
+        if (Blacklist is {EnabledSetting.Value: true, ModeSetting.Value: BlacklistMode.Inclusion} && !InFilteredZone()) return true;
+
+        if (Condition.IsDutyRecorderPlayback() && Settings.DisableDuringDutyRecorderPlaybackSetting.Value) return true;
+        if (Condition.IsBoundByDuty() && Settings.DisableDuringDutySetting.Value) return true;
+        if (Condition.IsInCombat() && Settings.DisableDuringCombatSetting.Value) return true;
+        if (Condition.IsInCutscene() && Settings.DisableDuringCutsceneSetting.Value) return true;
+        if (Condition.IsInQuestEvent() && Settings.DisableDuringQuestEventSetting.Value) return true;
+        if (Condition.IsCrafting() && Settings.DisableDuringCraftingSetting.Value) return true;
+        if (Condition.IsInIslandSanctuary() && Settings.DisableIslandSanctuarySetting.Value) return true;
+        if (Condition.IsBetweenAreas()) return true;
+
+        return false;
+    }
+
+    private static bool InFilteredZone()
+    {
+        return Blacklist.BlacklistedZones.Value.Any(territory => territory == Service.ClientState.TerritoryType);
+    }
+}
